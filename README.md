a87c32187fdac7f91a3dd95fa6c4d8fa80110c5f
Joel Ross <joelross@uw.edu>
2017-05-19 11:16:15 -0700

    Add starter code

---
+++ README.md
@@ -0,0 +1,3 @@
+# a8 - D3 Visualization
+
+This repository contains an online, interactive visualization for a particular data set, created as part of an assignment for the [Introduction to Programming](https://canvas.uw.edu/courses/1139975) course at the UW iSchool.
---
+++ visualization.js
@@ -0,0 +1,10 @@
+'use strict';  //treat silly mistakes as run-time errors
+
+//the SVG element to add visual content to
+var svg = d3.select('#visContainer')
+		.append('svg')
+		.attr('height', 480) //can adjust size as desired
+		.attr('width', 600)
+    .style('border','1px solid gray'); //comment out to remove border
+
+/* Your script goes here */
